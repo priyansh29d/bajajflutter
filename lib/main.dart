dac43ef90cb9d2aa2bb5a9d5d1b398101cb07075
priyansh29d <priyash929@gmail.com>
2022-02-03 12:20:44 +0530

    Body And Button

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-                title: Text("App Bar")
-    )
-  )));
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App Bar'),
+          centerTitle: true,
+        ),
+        body: Center(child: Text('Welcome')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          child: Text('Click'),
+        ),
+      ),
+    ),
+  );
 }
